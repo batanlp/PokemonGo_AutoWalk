dab18cd7ed29398ed9c9363bc2a423e068077f5f
Nguyen Tan <batanlp@yahoo.com>
2017-07-27 14:21:02 +0700

    Init project

---
+++ Pokemongo_Autowalk/Pokemongo_Autowalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemongo_Autowalk
+//
+//  Created by Nguyen Tan on 7/27/17.
+//  Copyright © 2017 Nguyen Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
